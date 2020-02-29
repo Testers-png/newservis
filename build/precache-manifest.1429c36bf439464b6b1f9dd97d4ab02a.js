a5d4569781f649ee53e920d850d4a23fc6a838d7
Testers-png <61288366+Testers-png@users.noreply.github.com>
2020-02-29 20:30:23 +0300

    Add files via upload

---
+++ build/precache-manifest.1429c36bf439464b6b1f9dd97d4ab02a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fa818cb6b56775d60222fca10f7a0f",
+    "url": "/newservis/index.html"
+  },
+  {
+    "revision": "2ce62d80cd418adf3831",
+    "url": "/newservis/static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "098d9cf7c148f0700dbc",
+    "url": "/newservis/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2ce62d80cd418adf3831",
+    "url": "/newservis/static/js/2.92d5dc89.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/newservis/static/js/2.92d5dc89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "098d9cf7c148f0700dbc",
+    "url": "/newservis/static/js/main.88242e1f.chunk.js"
+  },
+  {
+    "revision": "1956adeb092df914c1d9",
+    "url": "/newservis/static/js/runtime-main.df026763.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/newservis/static/media/persik.4e1ec840.png"
+  }
+]);
